3e2ffada766aa59d8e43779ee9ee8150d12eaba0
Arisandi <arisandisatria541@gmail.com>
2023-10-03 09:52:41 +0700

    connect to database

--- api/config.js
+++ api/config.js
@@ -1,1 +1,4 @@
+import "dotenv/config.js"
+
 export const PORT = 3000
+export const mongoDBURL = process.env.DB_CONN
--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
-import express from 'express'
-import { PORT } from './config.js';
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
-})
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+  })
+  .catch((error) => {
+    console.log(error);
+  });

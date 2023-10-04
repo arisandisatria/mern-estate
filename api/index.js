835aaa8e635b4eb065f98faabf91760b6a0d773b
Arisandi <arisandisatria541@gmail.com>
2023-10-04 21:26:14 +0700

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 const app = express();
+
+app.use(express.json())
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
@@ -20,3 +23,4 @@ mongoose
 
 
 app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)

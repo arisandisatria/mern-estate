720ba83b99f2ac9382179167ae9f5dbec265b11b
Arisandi <arisandisatria541@gmail.com>
2023-11-25 17:14:42 +0700

    create the listing item component and show listings

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -98,10 +98,9 @@ export default function Listing() {
             <p className="text-2xl font-semibold">
               {listing.name} -${" "}
               {listing.offer
-                ? // ? listing.discountPrice.toLocaleString('en-US')
-                  // : listing.regulerPrice.toLocaleString('en-US')
-                  listing.discountPrice
-                : listing.regulerPrice}
+                ? listing.discountPrice?.toLocaleString('en-US')
+                : listing.regulerPrice?.toLocaleString('en-US')
+              }
               {listing.type == "rent" && " / month"}
             </p>
             <p className="flex items-center mt-6 gap-2 text-slate-600 text-sm">
@@ -114,7 +113,7 @@ export default function Listing() {
               </p>
               {listing.offer && (
                 <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
-                  ${+listing.regulerPrice - listing.discountPrice}
+                  ${(+listing.regulerPrice - listing.discountPrice)?.toLocaleString('en-US')} Discount
                 </p>
               )}
             </div>
